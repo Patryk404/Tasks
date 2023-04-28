d2aed72930ab5d7068fa9e746b38161e4d0f5377
Patryk <patrykkurek@student.agh.edu.pl>
2023-04-28 15:14:20 +0200

    Added execution time

--- prime/task.js
+++ prime/task.js
@@ -1,3 +1,5 @@
+
+
 const is_prime = (num)=> {
   if (num <= 1) {
     return false;
@@ -25,5 +27,6 @@ const task = (tab1,tab2)=>{
     }); 
     return tab3;
 }
-
+console.time("Execution Time");
 console.log(task([2,3,9,2,5,1,3,7,10],[2,1,3,4,3,10,6,6,1,7,10,10,10]));
+console.timeEnd("Execution Time"); 
